f477096a11cb2a840888527d757bc6c636f346bf
王重阳 <13149854342@163.com>
2017-11-13 16:08:13 +0800

    add homework-todo for RegularMatch

---
+++ src/findnumberinarray/FindNumber.java
@@ -0,0 +1,32 @@
+package findnumberinarray;
+
+public class FindNumber {
+
+    public static void main(String[] args) {
+
+        int[][] array = new int[][]{
+                {1, 2, 8, 9},
+                {2, 4, 9, 12},
+                {4, 7, 10, 13},
+                {6, 8, 11, 15}
+        };
+
+        int targetNum = 5;
+        System.out.println(findNumber(array, targetNum));
+    }
+
+    private static boolean findNumber(int[][] array, int targetNum) {
+
+        for (int[] anArray : array) {
+            for (int anAnArray : anArray) {
+                if (anAnArray > targetNum) {
+                    break;
+                } else if (anAnArray == targetNum) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
